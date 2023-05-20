750aa1f30e9916d071a4639d7d21bfce230d7cd5
Hamid HaghPanah <hhaghpanah16@gmail.com>
2023-05-20 20:11:53 +0330

    Add files via upload

---
+++ todo-app/src/components/Todo.js
@@ -0,0 +1,15 @@
+import { faPenToSquare, faTrashCan } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+
+export const Todo = ({task, toggleComplete, deleteTodo, editTodo}) => {
+  return (
+    <div className='Todo'>
+        <p onClick={() => toggleComplete(task.id)} className={`${task.completed ? 'completed' : ''}`}>{task.task}</p>
+        <div>
+            <FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(task.id)}/>
+            <FontAwesomeIcon icon={faTrashCan} onClick={()=> deleteTodo(task.id)}/>
+        </div>
+    </div>
+  )
+}
